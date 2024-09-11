e44372aa2547f2e6acaee613a2191423cf622621
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-09-11 16:08:19 +0200

    Initial commit

---
+++ src/main/java/cristianorocchi/u5d8/entities/Autore.java
@@ -0,0 +1,25 @@
+package cristianorocchi.u5d8.entities;
+
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "Autori")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Autore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private int id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String dataDiNascita;
+    private String avatar;
+}
---
+++ src/main/java/cristianorocchi/u5d8/entities/Post.java
@@ -0,0 +1,28 @@
+package cristianorocchi.u5d8.entities;
+
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String categoria;
+    private String titolo;
+    private String cover;
+    private String contenuto;
+    private int tempoDiLettura;
+
+    @ManyToOne
+    @JoinColumn(name = "autore_id", nullable = false)
+    private Autore autore;
+}
